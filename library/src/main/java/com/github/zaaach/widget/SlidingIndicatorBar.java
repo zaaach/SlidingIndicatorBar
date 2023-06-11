9b0ccf023248cc5a6222e85a26ba3ef26c64debe
zaaach <1143017942@qq.com>
2023-06-11 15:46:35 +0800

    *

--- library/src/main/java/com/github/zaaach/widget/SlidingIndicatorBar.java
+++ library/src/main/java/com/github/zaaach/widget/SlidingIndicatorBar.java
@@ -11,8 +11,12 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * @Author: Zaaach
@@ -24,10 +28,18 @@ public class SlidingIndicatorBar extends View {
     private int barColor;
     private int bendingHeight;
     private float bendingRatio;
+    private int bendingDirection;
     private Paint barPaint;
     private Paint circlePaint;
     private final RectF rectF = new RectF();
     private final Path path = new Path();
+
+    @IntDef(value = {Direction.DOWN, Direction.UP})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Direction {
+        int DOWN = 0;
+        int UP = 1;
+    }
 
     public SlidingIndicatorBar(Context context) {
         this(context, null);
@@ -54,6 +66,7 @@ public class SlidingIndicatorBar extends View {
         barColor = array.getColor(R.styleable.SlidingIndicatorBar_sib_bar_color, Color.LTGRAY);
         bendingHeight = array.getDimensionPixelSize(R.styleable.SlidingIndicatorBar_sib_bending_height, 60);
         bendingRatio = array.getFloat(R.styleable.SlidingIndicatorBar_sib_bending_ratio, 0);
+        bendingDirection = array.getInt(R.styleable.SlidingIndicatorBar_sib_bending_direction, Direction.DOWN);
         array.recycle();
 
         barPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -96,19 +109,42 @@ public class SlidingIndicatorBar extends View {
             barPaint.setStyle(Paint.Style.FILL);
             canvas.drawRoundRect(rectF, radius, radius, barPaint);
         } else {
-            path.reset();
-            path.moveTo(getPaddingLeft() + radius, getPaddingTop() + radius);
-            float x = getPaddingLeft() + (getWidth() - getPaddingLeft() - getPaddingRight()) / 2f;
-            float y = getPaddingTop() + barHeight / 2f + bendingHeight * bendingRatio;
-            path.lineTo(x, y);
-            path.lineTo(getWidth() - getPaddingRight() - radius, getPaddingTop() + radius);
-            barPaint.setStyle(Paint.Style.STROKE);
-            canvas.drawPath(path, barPaint);
-            canvas.drawCircle(getPaddingLeft() + radius, getPaddingTop() + radius,
-                    radius, circlePaint);
-            canvas.drawCircle(getWidth() - getPaddingRight() - radius, getPaddingTop() + radius,
-                    radius, circlePaint);
+            if (bendingDirection == Direction.UP) {
+                drawArrowUp(canvas, radius);
+            } else {
+                drawArrowDown(canvas, radius);
+            }
         }
+    }
+
+    private void drawArrowDown(Canvas canvas, float radius) {
+        path.reset();
+        path.moveTo(getPaddingLeft() + radius, getPaddingTop() + radius);
+        float x = getPaddingLeft() + (getWidth() - getPaddingLeft() - getPaddingRight()) / 2f;
+        float y = getPaddingTop() + barHeight / 2f + bendingHeight * bendingRatio;
+        path.lineTo(x, y);
+        path.lineTo(getWidth() - getPaddingRight() - radius, getPaddingTop() + radius);
+        barPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, barPaint);
+        canvas.drawCircle(getPaddingLeft() + radius, getPaddingTop() + radius,
+                radius, circlePaint);
+        canvas.drawCircle(getWidth() - getPaddingRight() - radius, getPaddingTop() + radius,
+                radius, circlePaint);
+    }
+
+    private void drawArrowUp(Canvas canvas, float radius) {
+        path.reset();
+        path.moveTo(getPaddingLeft() + radius, getHeight() - getPaddingBottom() - radius);
+        float x = getPaddingLeft() + (getWidth() - getPaddingLeft() - getPaddingRight()) / 2f;
+        float y = getPaddingTop() + barHeight / 2f;
+        path.lineTo(x, y);
+        path.lineTo(getWidth() - getPaddingRight() - radius, getHeight() - getPaddingBottom() - radius);
+        barPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, barPaint);
+        canvas.drawCircle(getPaddingLeft() + radius, getHeight() - getPaddingBottom() - radius,
+                radius, circlePaint);
+        canvas.drawCircle(getWidth() - getPaddingRight() - radius, getHeight() - getPaddingBottom() - radius,
+                radius, circlePaint);
     }
 
     /**
